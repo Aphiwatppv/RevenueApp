ddfce724d0444effdaa37f71f9a49d51fcff518b
Aphiwat Deelorm <AphiwatPPV@outlook.com>
2024-04-26 21:15:44 +0700

    First Update

---
+++ DailyRecordRevenueApp/DailyRecordRevenueApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace DailyRecordRevenueApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RevenueSystemBacked/DataAccessOldFrameWork4_6/SqlAccess/SqlAccessInternal.cs
@@ -0,0 +1,38 @@
+﻿using Dapper; // V1.6
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DataAccessOldFrameWork4_6.SqlAccess
+{
+    public class SqlAccessInternal
+    {
+        private readonly string _connectionstring;
+
+        public SqlAccessInternal(string connectionstring)
+        {
+            _connectionstring = connectionstring;
+        }
+
+
+        public async Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, U Parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionstring))
+            {
+                return await connection.QueryAsync<T>(storedProcedure, Parameters, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+
+
+        public async Task UpdateAsync<T>(string storedProcedure, T Parameters)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionstring))
+            {
+                await connection.ExecuteAsync(storedProcedure, Parameters, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
